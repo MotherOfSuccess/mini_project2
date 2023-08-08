6a3dd198351ddb355d54d9e6b3ad28b61799f56e
MotherOfSuccess <trinhkhoa247@gmail.com>
2023-08-08 11:01:40 +0700

    update

--- src/modules/blog/dtos/update-blog.dto.ts
+++ src/modules/blog/dtos/update-blog.dto.ts
@@ -1,12 +1,18 @@
+import { Transform } from "class-transformer"
+import { IsBiggerThan } from "src/decorators/decorator"
 
 export class UpdateBlogDto {
 
+    @IsBiggerThan(0)
     readonly id_author?: number
 
+    @Transform(({value}) => value?.toString().trim())
     readonly name?: string
 
+    @IsBiggerThan(0)
     readonly categoryid?: number
 
+    @Transform(({value}) => value?.toString().trim())
     readonly content?: string
 
     readonly image?: string[]
