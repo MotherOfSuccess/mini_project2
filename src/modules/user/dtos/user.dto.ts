a28c46cdee16c7eefb84c20a9326a54c5008a93b
MotherOfSuccess <trinhkhoa247@gmail.com>
2023-08-08 10:21:27 +0700

    Update

--- src/modules/user/dtos/user.dto.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    readonly id: string
-    readonly name: string
-    readonly gender: string
-    readonly age: number
-    readonly password: string
-    readonly refreshToken?: string
-    readonly accessToken?: string
-}
